d7adf87a85bab9d44dda471ba8a3d4ff96d397a2
Renê <renejr.arraes286@gmail.com>
2020-11-05 08:40:38 -0300

    working with vars and types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  public todos: any[] = [];
+
+  constructor(){
+    this.todos.push('Estudar');
+    this.todos.push('Lavar os pratos');
+    this.todos.push('Lavar a casa');
+  }
 }
